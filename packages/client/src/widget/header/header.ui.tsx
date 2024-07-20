14a4e3e0b4116733b757cf8b5086bc8b8acef060
qpoiop <qpoiop3@gmail.com>
2024-07-20 15:12:58 +0900

    FEAT. change shared layout path

--- packages/client/src/widget/header/header.ui.tsx
+++ packages/client/src/widget/header/header.ui.tsx
@@ -1,16 +1,16 @@
-import { Header as CoreUIHeader, Icon } from '@/shared/lib'
+import { Header as CoreUIHeader } from '@/shared/layouts'
 import React, { ElementType, SVGProps, useMemo } from 'react'
-import MainLogo from '/public/images/logo.svg?react'
+// import MainLogo from '/public/images/logo.svg?react'
 
 function headerRoot() {
-    const logoContent = useMemo(() => {
-        return <Icon source={MainLogo as ElementType<SVGProps<SVGSVGElement>>} />
-    }, [])
+    // const logoContent = useMemo(() => {
+    //     return <Icon source={MainLogo as ElementType<SVGProps<SVGSVGElement>>} />
+    // }, [])
 
     return (
-        <CoreUIHeader leftContent={logoContent}>
-            <div></div>
-        </CoreUIHeader>
+        // <CoreUIHeader leftContent={logoContent}>
+        //     <div></div>
+        // </CoreUIHeader>
     )
 }
 
